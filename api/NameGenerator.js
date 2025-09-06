bc9ce62d952d79060e5f7bb599e5ffd1ea54fd3e
goodboys21 <akunceeingood5@gmail.com>
2025-09-06 18:52:54 +0700

    Update and rename namegenerator.js to api/NameGenerator.js

--- api/NameGenerator.js
+++ api/NameGenerator.js
@@ -8,17 +8,16 @@ class NameGenerator {
   gen_mail() {
     const user = this._first[Math.floor(Math.random() * this._first.length)];
     const domain = this._last[Math.floor(Math.random() * this._last.length)];
-    const number = Math.floor(Math.random() * 1000); // 0-999
+    const number = Math.floor(Math.random() * 1000);
     return `${user}${number}${domain}`;
   }
 
   gen_pass() {
     const pass_user = this._first[Math.floor(Math.random() * this._first.length)];
-    const number = Math.floor(Math.random() * 10000); // 0-9999
+    const number = Math.floor(Math.random() * 10000);
     return `${pass_user}${number}`;
   }
 
-  // Generate 1 fake IP (IPv4)
   gen_ip() {
     return Array.from({ length: 4 }, () => Math.floor(Math.random() * 256)).join('.');
   }
